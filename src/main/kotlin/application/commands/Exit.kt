8edeb46e5a615904e20894c16b622dcb16374a4b
dmchq <gdima617@gmail.com>
2026-02-22 23:59:37 +0300

    feature(app): rewrite exit command code

--- src/main/kotlin/application/commands/Exit.kt
+++ src/main/kotlin/application/commands/Exit.kt
@@ -1,7 +1,8 @@
 package application.commands
 
 import application.Handler
-import kotlin.system.exitProcess
+import application.ScriptExecutor
+import application.exceptions.ExitSignal
 
 class Exit (
     override val app: Handler
@@ -10,8 +11,9 @@ class Exit (
     override val description = "Завершает программу (без записи файла в коллекцию)"
 
     override fun execute(argument: String) {
-        val io = app.io
-        io.printLine("Программа завершена")
-        exitProcess(0)
+        val message = if (app is ScriptExecutor) {
+            "Сигнал выхода вызван из скрипта ${app.pathName}"
+        } else "Завершение программы, удаление коллекции... Вы же не забыли её сохранить?... O_o"
+        throw ExitSignal(message)
     }
 }
