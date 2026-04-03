b3c11963bbf9950c40994eaeec849de10d127f4c
3fant <3fantimofey@gmail.com>
2026-04-03 19:04:30 +0300

    fix: api refactor

--- api/src/main/kotlin/RpcResponse.kt
+++ api/src/main/kotlin/RpcResponse.kt
@@ -3,6 +3,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 class RpcResponse(
     val code: ApiCodes,
-    val data: Map<String, String>,
-    val message: String?
+    val data: String
 )
