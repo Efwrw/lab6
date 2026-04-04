c0bd8bce0176973d549d3401b5f93b4d4c500747
dmchq <gdima617@gmail.com>
2026-04-04 08:28:29 +0300

    refactor: rewrite client container logic from sockets to socketchannels

--- client/src/main/kotlin/ClientContainer.kt
+++ client/src/main/kotlin/ClientContainer.kt
@@ -1,21 +1,30 @@
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.net.InetSocketAddress
 import java.net.Socket
+import java.nio.channels.SocketChannel
 
 class ClientContainer {
     val parser = Parser()
     val resolver = ViewResolver()
     val IO: IOPort = CliManager()
-    val client = Client(this)
-
-
-
+    val clientEnt = Client(this)
+    var socket: SocketChannel? = null
+    var channelIO: ChannelIO? = null
     fun up(){
-        val clientSocket = Socket("127.0.0.1", 5433)
-        val reader = InputStreamReader(clientSocket.getInputStream()).buffered()
-        val writer = OutputStreamWriter(clientSocket.getOutputStream()).buffered()
-        while (true) {
-            client.run(reader, writer)
+        val address = InetSocketAddress("127.0.0.1", 5432)
+        try {
+            val client = SocketChannel.open(address)
+            client.configureBlocking(true)
+            this.socket = client
+            this.channelIO = ChannelIO(client)
+            println(channelIO.toString())
+            while (true) {
+                clientEnt.run()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return
         }
     }
 }
