e403b3195bf004f8ed38f4916695e5641054d7cd
dmchq <gdima617@gmail.com>
2026-04-04 08:27:34 +0300

    feature: add channel io managers to client and server

---
+++ client/src/main/kotlin/ChannelIO.kt
@@ -0,0 +1,54 @@
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
+
+class ChannelIO(
+    private val channel: SocketChannel,
+) {
+    private var size = -1
+    private val sizeBuffer = ByteBuffer.allocate(4)
+    private var dataBuffer: ByteBuffer? = null
+
+    fun read(): RpcResponse? {
+        if (size == -1) {
+            val bytesRead = channel.read(sizeBuffer)
+            if (bytesRead == -1) throw Exception("Channel closed")
+            if (sizeBuffer.hasRemaining()) return null
+
+            sizeBuffer.flip()
+            size = sizeBuffer.int
+            sizeBuffer.clear()
+
+            dataBuffer = ByteBuffer.allocate(size)
+        }
+
+        val bytesReadData = channel.read(dataBuffer!!)
+        if (bytesReadData == -1) throw Exception("Channel closed")
+
+        if (dataBuffer!!.hasRemaining()) return null
+
+        val json = String(dataBuffer!!.array(), Charsets.UTF_8)
+        val rpc = Json.decodeFromString<RpcResponse>(json)
+
+        size = -1
+        dataBuffer = null
+
+        return rpc
+    }
+
+    fun write(message: RpcRequest) {
+        val json = Json.encodeToString(message)
+        val bodyBytes = json.toByteArray(Charsets.UTF_8)
+
+        val writeBuffer = ByteBuffer.allocate(4 + bodyBytes.size)
+        writeBuffer.putInt(bodyBytes.size)
+        writeBuffer.put(bodyBytes)
+        writeBuffer.flip()
+
+        while (writeBuffer.hasRemaining()) {
+            val written = channel.write(writeBuffer)
+            if (written == -1) throw Exception("Disconnected while writing")
+        }
+    }
+}
