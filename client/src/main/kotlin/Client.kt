0e38a266dd12559c042a332ad1b429da6039d0d5
dmchq <gdima617@gmail.com>
2026-04-17 21:09:08 +0300

    feature(client): add client response resolver part inside Client entity

--- client/src/main/kotlin/Client.kt
+++ client/src/main/kotlin/Client.kt
@@ -3,17 +3,16 @@ class Client(val clientContainer: ClientContainer) {
     val resolver = clientContainer.resolver
     val parser = clientContainer.parser
     fun run() {
-        val channelIO = clientContainer.channelIO
-        io.printBefore("> ")
-        val request = parser.parse()
-
-
-        channelIO.write(request)
-        val responseFromJson = channelIO.read() ?: return
-
-        val resolvedResponse = resolver.resolve(responseFromJson)
-        io.printLine(resolvedResponse)
-
-
+        try {
+            val channelIO = clientContainer.channelIO
+            io.printBefore("> ")
+            val input = io.readLine()
+            if (input != null) {
+                val request = clientContainer.invoker.resolveCommand(input.trim())
+                channelIO.write(request)
+            }
+            val responseFromJson = channelIO.read() ?: return
+            resolver.resolve(responseFromJson)
+        } catch (e: Exception) {}
     }
 }
