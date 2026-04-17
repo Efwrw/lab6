36f2d4cf96b770d12a7799e89633df5f0f1f4709
dmchq <gdima617@gmail.com>
2026-04-17 21:05:47 +0300

    refactor: remake organization builder logic

--- server/src/main/kotlin/application/OrganizationBuilder.kt
+++ server/src/main/kotlin/application/OrganizationBuilder.kt
@@ -7,16 +7,16 @@ import domain.Organization
 import domain.OrganizationType
 import java.time.LocalDate
 
-fun buildOrganization(cm: CollectionManager, data: Map<String, String>): Organization {
-    val name = data["name"]?.trim()
-    val x = data["x"]?.trim()?.toFloat()
-    val y = data["y"]?.trim()?.toFloat()
-    val turnover = data["turnover"]?.trim()?.toFloat()
-    val fullName = data["fullName"]?.trim()
-    val empCount = data["empCount"]?.trim()?.toLongOrNull()
-    val street = data["street"]?.trim()
-    val zip = data["zip"]?.trim()
-    val type = data["type"]?.trim()?.lowercase()
+fun buildOrganization(cm: CollectionManager, data: List<String>): Organization {
+    val name = data[0].trim()
+    val x = data[1].trim().toFloat()
+    val y = data[2].trim().toFloat()
+    val turnover = data[3].trim().toFloat()
+    val fullName = data[4].trim()
+    val empCount = data[5].trim().toLongOrNull()
+    val street = data[6].trim()
+    val zip = data[7].trim()
+    val type = data[8].trim().lowercase()
     requireNotNull(name) {"Name is required"}
     requireNotNull(turnover) {"Turnover is required"}
     requireNotNull(fullName) {"Full name is required"}
