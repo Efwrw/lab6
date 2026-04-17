4c44256f6874cda6ab0310b38801406c1dbe1fb5
dmchq <gdima617@gmail.com>
2026-04-17 22:42:44 +0300

    feature(client): add exit and help command to client side

---
+++ client/src/main/kotlin/commands/Help.kt
@@ -0,0 +1,15 @@
+package commands
+
+import ClientContainer
+
+class Help: Command {
+    override val name: String = "help"
+    override val description: String = "Выводит справку о текущих командах."
+    override fun execute(context: ClientContainer){
+        val io = context.IO
+        val invoker = context.invoker
+        val strBuilder = StringBuilder()
+        invoker.serverCommands.forEach { strBuilder.append("${it.key} - ${it.value.description}\n") }
+        io.printLine(strBuilder.toString())
+    }
+}
