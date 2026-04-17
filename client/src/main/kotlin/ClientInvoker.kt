441dc7f823f30b0b428ad673beb6e000b73105fc
dmchq <gdima617@gmail.com>
2026-04-17 22:45:10 +0300

    feature(client): add client commands to invoker

--- client/src/main/kotlin/ClientInvoker.kt
+++ client/src/main/kotlin/ClientInvoker.kt
@@ -1,36 +1,51 @@
 import command.CommandSyntax
+import commands.Command
+import commands.Exit
+import commands.Help
 
 class ClientInvoker(
     private val context: ClientContainer,
 ) {
-    val commands = HashMap<String, CommandSyntax>()
-
-    fun resolveCommand(command: String): Request {
-        val syntax = getSyntax(command)
-        val args = mutableListOf<String>()
-        for (string in syntax) {
-            context.IO.printBefore("$string:")
-            val arg = context.IO.readLine()
-            if (arg != null) {
-                args.add(arg)
+    val serverCommands = HashMap<String, CommandSyntax>()
+    val clientCommands = HashMap<String, Command>()
+    init {
+        clientCommands["help"] = Help()
+        clientCommands["exit"] = Exit()
+    }
+    fun resolveCommand(command: String): Request? {
+        val clientCommand = clientCommands[command]
+        val serverCommand = serverCommands[command]
+        if (serverCommand != null) {
+            val syntax = getSyntax(command)
+            val args = mutableListOf<String>()
+            for (string in syntax) {
+                context.IO.printBefore("$string:")
+                val arg = context.IO.readLine()
+                if (arg != null) {
+                    args.add(arg)
+                }
             }
+            return Request.ExecuteCommand(command, args)
         }
-
-        return Request.ExecuteCommand(command, args)
-
+        clientCommand?.execute(context)
+        return null
     }
 
     fun getSyntax(name: String): List<String> {
-        println()
-        val command = commands[name] ?: throw IllegalArgumentException("Неизвестная комманда: $name")
+        val command = serverCommands[name] ?: throw IllegalArgumentException("Неизвестная комманда: $name")
         return command.args
     }
 
     fun load(source: Response.HandShake){
         val commandsSyntax = source.commands
-        commandsSyntax.forEach {commands[it.name] = it}
+        commandsSyntax.forEach {serverCommands[it.name] = it}
     }
+
+    fun addCommand(command: Command){
+        clientCommands[command.name] = command
+    }
+
     fun clear(){
-        commands.clear()
+        serverCommands.clear()
     }
 }
