1781aeea461b6bec07f333643088123b082f80c0
3fant <3fantimofey@gmail.com>
2026-04-18 00:25:43 +0300

    feat: script related part add in invoker

--- client/src/main/kotlin/ClientInvoker.kt
+++ client/src/main/kotlin/ClientInvoker.kt
@@ -1,5 +1,6 @@
 import command.CommandSyntax
 import commands.Command
+import commands.ExecuteScript
 import commands.Exit
 import commands.Help
 
@@ -8,18 +9,33 @@ class ClientInvoker(
 ) {
     val serverCommands = HashMap<String, CommandSyntax>()
     val clientCommands = HashMap<String, Command>()
+
     init {
         clientCommands["help"] = Help()
         clientCommands["exit"] = Exit()
+        clientCommands["execute_script"] = ExecuteScript()
     }
+
     fun resolveCommand(command: String): Request? {
-        val clientCommand = clientCommands[command]
-        val serverCommand = serverCommands[command]
-        if (serverCommand != null) {
+        clientCommands[command]?.let {
+            if (command == "execute_script") {
+                val name = context.IO.readLine()
+                val scriptManager = context.scriptManager
+
+                scriptManager.add(name ?: error("expected name"))
+
+                if (!scriptManager.isRunning) {
+                    resolveScript()
+                }
+            }
+            clientCommands[command]!!.execute(context)
+            return null
+        }
+        serverCommands[command]?.let {
             val syntax = getSyntax(command)
             val args = mutableListOf<String>()
             for (string in syntax) {
-                context.IO.printBefore("$string:")
+                context.IO.printBefore("$string: ")
                 val arg = context.IO.readLine()
                 if (arg != null) {
                     args.add(arg)
@@ -27,8 +43,40 @@ class ClientInvoker(
             }
             return Request.ExecuteCommand(command, args)
         }
-        clientCommand?.execute(context)
-        return null
+
+        throw IllegalArgumentException("Неизвестная комманда: $command")
+    }
+
+    private fun resolveScript() {
+        val scriptManager = context.scriptManager
+        scriptManager.isRunning = true
+        while (scriptManager.buffer.isNotEmpty()) {
+            val command = scriptManager.getLine()
+            if (command == "execute_script") {
+                val name = scriptManager.getLine()
+                scriptManager.add(name)
+            } else {
+                val syntax = getSyntax(command)
+                val args = mutableListOf<String>()
+                for (i in syntax) {
+                    val arg = scriptManager.getLine()
+                    if (arg != "") {
+                        args.add(arg)
+                    }
+                }
+                val req = Request.ExecuteCommand(command, args)
+                context.channelIO.write(req)
+
+                val responseFromJson = context.channelIO.read() ?: return
+                if (responseFromJson is Response.Error) {
+                    scriptManager.panic()
+                    throw ScriptError(responseFromJson.message)
+                }
+                context.resolver.resolve(responseFromJson)
+            }
+        }
+        scriptManager.isRunning = false
+
     }
 
     fun getSyntax(name: String): List<String> {
@@ -36,16 +84,9 @@ class ClientInvoker(
         return command.args
     }
 
-    fun load(source: Response.HandShake){
+    fun load(source: Response.HandShake) {
         val commandsSyntax = source.commands
-        commandsSyntax.forEach {serverCommands[it.name] = it}
+        commandsSyntax.forEach { serverCommands[it.name] = it }
     }
 
-    fun addCommand(command: Command){
-        clientCommands[command.name] = command
-    }
-
-    fun clear(){
-        serverCommands.clear()
-    }
 }
