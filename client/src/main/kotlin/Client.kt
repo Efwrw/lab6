11f401a52617b91d49b9b8e76df2a3c1ae074ca3
3fant <3fantimofey@gmail.com>
2026-04-18 00:25:13 +0300

    fix: error handling add

--- client/src/main/kotlin/Client.kt
+++ client/src/main/kotlin/Client.kt
@@ -1,16 +1,21 @@
 class Client(val clientContainer: ClientContainer) {
     val io = clientContainer.IO
     val resolver = clientContainer.resolver
-    val parser = clientContainer.parser
     fun run() {
+        try {
             val channelIO = clientContainer.channelIO
             io.printBefore("> ")
             val input = io.readLine()
-            if (input != null) {
+            input?.let{
                 val request = clientContainer.invoker.resolveCommand(input.trim()) ?: return
                 channelIO.write(request)
             }
             val responseFromJson = channelIO.read() ?: return
             resolver.resolve(responseFromJson)
+        } catch (e: ScriptError) {
+            io.printLine("ошибка выполнения скрипта" + e.message)
+        } catch (e: IllegalArgumentException){
+            io.printLine(e.message)
+        }
     }
 }
