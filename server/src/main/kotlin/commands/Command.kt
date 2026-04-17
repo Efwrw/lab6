64567a21f1e15d70d74769c3a8a51d26fadc858b
dmchq <gdima617@gmail.com>
2026-04-17 21:04:23 +0300

    feature: add new method to command interface

--- server/src/main/kotlin/commands/Command.kt
+++ server/src/main/kotlin/commands/Command.kt
@@ -1,10 +1,15 @@
 package commands
 
+import Response
 import ServerContainer
+import command.CommandSyntax
 
 interface Command {
-    val container: ServerContainer
     val name: String
+    val args: List<String>
     val description: String
-    fun execute(args: List<String>, data: Map<String, String>): String
+    fun execute(context: ServerContainer, args: List<String>): Response
+    fun getSyntax(): CommandSyntax {
+        return CommandSyntax(name, args, description)
+    }
 }
