be7288bd22f1ecbd87e04bb4515aa41b3cb8f263
dmchq <gdima617@gmail.com>
2026-05-02 14:37:00 +0300

    refactor(server): rewrite start function in dsl

--- server/src/main/kotlin/ServerMain.kt
+++ server/src/main/kotlin/ServerMain.kt
@@ -1,13 +1,7 @@
 import commands.inner.ExitSignal
 
 fun main(args: Array<String>) {
-    var filePath: String = ""
-    if (args.isNotEmpty()) {
-        filePath = args[0]
-    }
-    val serverContainer = ServerContainer(filePath)
-    try{serverContainer.up()}
-    catch(e: ExitSignal){
-        println("сервер выключается")
+    serverContainer(args = args) {
+        up()
     }
 }
